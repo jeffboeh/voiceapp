528d7015965e4cd4ddde33c0be3471db4dfdb1f5
Jeff Boeh <Jeff@theironyard.com>
2017-05-12 14:47:32 -0400

    added 2016 artist to jquery autocomplete

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 var names = [
-
+        { value: 'Aaron Gibson', data: 'aarongibson' },
+    { value: 'Ali Caldwell', data: 'alicaldwell' },
     { value: 'Aliyah Moulden', data: 'aliyahmoulden' },
     { value: 'Brennley Brown', data: 'brennleybrown' },
+      { value: 'Brenden Fletcher', data: 'brendanfletcher' },
+     { value: 'Billy Gilman', data: 'billygilman' },
      { value: 'Chris Blue', data: 'chrisblue' },
+     { value: 'Christian Cuevas', data: 'christiancuevas' },
      { value: 'Hunter Plake', data: 'hunterplake' },
-    { value: 'Jesse Larson', data: 'jesselarson' },
+     { value: 'Jesse Larson', data: 'jesselarson' },
+    { value: 'Josh Gallagher', data: 'joshgallagher' },
     { value: 'Lauren Duski', data: 'laurenduski' },
+     { value: 'Sundance Head', data: 'sundancehead' },
     { value: 'Tsoul', data: 'tsoul' },
     { value: 'Vanessa Ferguson', data: 'vanessaferguson.html' },
+    { value: 'We McDonald', data: 'wemcDonald' },
 ];
 
 $('#autocomplete').autocomplete({
